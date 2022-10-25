65ba571f5ac652a2049fbe5f1b22fa0382829ccf
qazwaz <edwardtuzimek@gmail.com>
2022-10-25 16:25:04 +0200

    configuration and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Routes, Route, BrowserRouter, } from "react-router-dom";
+import Home from "./components/Home"
 
 function App() {
   return (
-    <>
-
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />              //musze zmienic na swoje podstrony
+          <Route path="/register" element={<Register />} /> */}
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/header/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='container navbar'>
+            <ul className='navbar__list'>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Skills</li>
+                <li>Contact</li>
+                <li>Icons</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
